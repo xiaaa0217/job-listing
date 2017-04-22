b4b97440d4a432279fda5e5ffb666af970e4b9a1
xiaaa <xiaenchanter@gmail.com>
2017-04-22 23:58:48 +0800

    implement part4

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
 before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
   end
 
   def show
@@ -13,7 +13,7 @@ before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destro
     @job = Job.new
   end
 
-  def created
+  def create
     @job = Job.new(job_params)
 
     if @job.save
@@ -47,6 +47,6 @@ before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destro
   private
 
   def  job_params
-    params.require(:job).permit(:title, :description)，:wage_upper_bound, :wage_lower_bound, :contact_email
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422135616) do
+ActiveRecord::Schema.define(version: 20170422140838) do
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -20,11 +20,12 @@ ActiveRecord::Schema.define(version: 20170422135616) do
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.integer  "wage_upper_bound"
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
+    t.boolean  "is_hidden",        default: true
   end
 
   create_table "users", force: :cascade do |t|
